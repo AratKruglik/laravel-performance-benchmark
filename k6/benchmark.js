5eeaabb2adc1a83b7c6b620abaab1dba252a8aec
Oleksii Kruhlyk <oleksii.kruhlyk@justeattakeaway.com>
2024-05-02 20:59:42 +0300

    Tests config init

--- k6/benchmark.js
+++ k6/benchmark.js
@@ -1,18 +1,26 @@
 import http from 'k6/http';
+import {sleep} from "k6";
 
 export const options = {
     insecureSkipTLSVerify: true,
-    iterations: 10000,
+    // iterations: 50000,
     // Key configurations for avg load test in this section
     stages: [
-        // { duration: '1m', target: 100 }, // traffic ramp-up from 1 to 1000 users over 1 minute.
+        { duration: '1m', target: 100 }, // traffic ramp-up from 1 to 1000 users over 1 minute.
         // { duration: '1m', target: 100 }, // stay at 100 users for 1 minute
         // { duration: '5m', target: 0 }, // ramp-down to 0 users
     ],
 };
 
 export default () => {
-    const response = http.get('https://localhost/api/users');
+    const params = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+    };
+
+    const response = http.get('http://localhost/api/users', params);
     // sleep(1);
     // MORE STEPS
     // Here you can have more steps or complex script
